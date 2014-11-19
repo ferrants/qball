a335905817e71d56435e16b928e8633e606a5a35
Matt Ferrante <mferrante@dataxu.com>
2014-11-19 18:47:41 -0500

    dockerize

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:0.10-onbuild
 MAINTAINER Matt Ferrante "mferrante@dataxu.com"
 RUN npm install -g coffee-script
+RUN ./node_modules/bower/bin/bower install --allow-root
 EXPOSE 8080
 CMD ["coffee", "server.coffee"]
--- init.sh
+++ init.sh
@@ -2,9 +2,3 @@
 
 npm install
 ./node_modules/bower/bin/bower install
-rm -rf public/lib/
-mkdir public/lib/
-ln -s ../../bower_components/angular/ public/lib/angular
-ln -s ../../bower_components/bootstrap/ public/lib/bootstrap
-ln -s ../../bower_components/jquery/ public/lib/jquery
-ln -s ../../bower_components/momentjs/ public/lib/momentjs
