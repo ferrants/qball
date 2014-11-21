980bf1d9d2229314d5a385af79e9782047283d88
Matt Ferrante <mferrante@dataxu.com>
2014-11-20 21:56:42 -0500

    mocha

--- runtest.sh
+++ runtest.sh
@@ -4,7 +4,7 @@ proc_num=$(ps aux | grep node | grep -v grep | wc -l)
 proc_num_s=$(echo $proc_num)
 if [ "$proc_num_s" = "0" ]; then
     mv balls.json balls.json.orig
-    mocha --reporter list --compilers coffee:coffee-script test/
+    ./node_modules/mocha/bin/mocha --reporter list --compilers coffee:coffee-script/register test/
     mv balls.json.orig balls.json
 else
 	echo "kill running server instances"
